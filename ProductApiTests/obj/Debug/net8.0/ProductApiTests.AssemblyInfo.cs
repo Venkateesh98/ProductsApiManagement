9dd145fbc4926e810af38348680facd1ba8a3cf1
Varadham <2213755@cognizant.com>
2025-05-14 07:26:24 +0530

    Initial commit with API and Test projects

--- ProductApiTests/obj/Debug/net8.0/ProductApiTests.AssemblyInfo.cs
+++ ProductApiTests/obj/Debug/net8.0/ProductApiTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApiTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26383d5cb18e46062a0c2754819f9f8059524bd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApiTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApiTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
